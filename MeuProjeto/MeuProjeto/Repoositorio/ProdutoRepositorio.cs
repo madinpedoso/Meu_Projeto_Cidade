05852c350e3361b421edd299ea7914729172b66b
Amanda <amandapcardoso981@gamil.com>
2025-05-13 22:16:17 -0300

    Criando cadastro do Produto

--- MeuProjeto/MeuProjeto/Repoositorio/ProdutoRepositorio.cs
+++ MeuProjeto/MeuProjeto/Repoositorio/ProdutoRepositorio.cs
@@ -1,6 +1,28 @@
-﻿ namespace MeuProjeto.Repoositorio
+﻿using MySql.Data.MySqlClient;
+using MeuProjeto.Models;
+using System.Data;
+
+namespace MeuProjeto.Repoositorio
 {
-    public class ProdutoRepositorio
+    public class ProdutoRepositorio(IConfiguration configuration)
     {
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+
+        public void Cadastrar(Produto produto)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("inset into produto (NomeP,DescricaoP,PresoP,QuantidadeP) values (@nomep,@descricaop,@presop,@quantidadep)", conexao);
+
+                cmd.Parameters.Add("@nomep", MySqlDbType.Varchar).Value = produto.NomeP;
+                cmd.Parameters.Add("@descricaop", MySqlDbType.Varchar).Value = produto.DescricaoP;
+                cmd.Parameters.Add("@presop", MySqlDbType.Varchar).Value = produto.PresoP; 
+                cmd.Parameters.Add("@quantidadep", MySqlDbType.Varchar).Value = produto.QuantidadeP;
+            }
+
+        }
+
     }
 }
